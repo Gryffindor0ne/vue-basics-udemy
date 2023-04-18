1e81910b5f5a5ac6cef33d6933f58b03a6d4b0d8
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-04-18 16:33:05 +0900

    Feat: watcher 사용하기

--- basic5/app.js
+++ basic5/app.js
@@ -3,16 +3,41 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
+      // fullname: "",
     };
+  },
+  watch: {
+    // data 프로퍼티와 같은 이름을 사용하면 해당 프로퍼티가 변경될 때마다 자동으로 실행된다.
+
+    counter(value) {
+      if (value > 50) {
+        this.counter = 0;
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullname = "";
+    //   } else {
+    //     this.fullname = this.name + " " + value;
+    //   }
+    // },
   },
   computed: {
     // 메서드이지만 데이터 프로퍼티처럼 사용한다.
     fullname() {
       console.log("running....");
-      if (this.name === "") {
+      if (this.name === "" || this.lastName === "") {
         return "";
       }
-      return this.name + " " + "Lee";
+      return this.name + " " + this.lastName;
     },
   },
   methods: {
@@ -34,6 +59,7 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = "";
+      this.lastName = "";
     },
   },
 });
