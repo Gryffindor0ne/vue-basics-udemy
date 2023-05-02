1bed47776c31b64b24255867b5918840c061d7e1
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-05-02 11:16:39 +0900

    Feat: Components 과제

---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label>Name</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Age</label>
+      <input type="number" v-model="enteredAge" />
+    </div>
+
+    <button>Add User</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emit: ["add-user"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+
+  methods: {
+    submitData() {
+      this.$emit("add-user", this.enteredName, this.enteredAge);
+      this.enteredName = "";
+      this.enteredAge = "";
+    },
+  },
+};
+</script>
