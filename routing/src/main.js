3570cb65c08a4270d4cf8d017419d8c230471c84
Ukjae Lee <79234473+Gryffindor0ne@users.noreply.github.com>
2023-05-12 20:35:35 +0900

    Feat: route 매개변수 사용하기

--- routing/src/main.js
+++ routing/src/main.js
@@ -4,6 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,6 +16,10 @@ const router = createRouter({
     {
       path: '/users',
       component: UsersList,
+    },
+    {
+      path: '/teams/:teamId',
+      component: TeamMembers,
     },
   ],
   linkActiveClass: 'active', // a 태그의 className 중 router-link-active 의 이름을 임의로 바꿔줄 수 있다.
